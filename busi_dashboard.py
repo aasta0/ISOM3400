e3aef9659e59014b27a118c7f69bcb8982992c14
aasta0 <aasta12122000@gmail.com>
2025-11-17 14:50:03 +0800

    Update busi_dashboard.py

--- busi_dashboard.py
+++ busi_dashboard.py
@@ -10,3 +10,6 @@ st.write("Please enter the monthly sales target and select the region.")
 
 sales_target = st.number_input("Enter Monthly Sales Target (in USD): ", value = 0)
 st.write(f"The monthly sales target (in USD) is {sales_target}.")
+
+region = st.selectbox("Select region:", ["Africa", "Antarctica", "Asia", "Australia", "Europe", "North America", "South America"])
+st.write(f"Your region is: {region}")
